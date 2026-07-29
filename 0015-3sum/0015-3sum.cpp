54a66f7ef0c1833c453f6dd18163f348dad86e62
Rajesh Runiwal <161800843+Notrajesh17@users.noreply.github.com>
2026-07-29 13:25:43 +0530

    Time: 54 ms (29.97%), Space: 30.8 MB (26.21%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,14 +3,16 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<vector<int>>ans;
         sort(nums.begin(), nums.end());
+
+        vector<vector<int>>ans;
 
         for(int i=0; i<n; i++)
         {
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j= i+1;
-            int k= n-1;
+            int j=i+1;
+            int k=n-1;
+
             while(j<k)
             {
                 int sum = nums[i]+nums[j]+nums[k];
@@ -25,7 +27,7 @@ public:
                 }
                 else
                 {
-                    vector<int>temp = {nums[i],nums[j], nums[k]};
+                    vector<int>temp({nums[i], nums[j], nums[k]});
                     ans.push_back(temp);
                     j++;
                     k--;
